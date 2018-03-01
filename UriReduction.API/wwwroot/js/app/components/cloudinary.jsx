934901109fdd25e18241f12b91bc6c2cb9795472
g.shuksto <g.shuksto@itransition.com>
2018-03-01 16:02:43 +0300

    Add react

--- UriReduction.API/wwwroot/js/app/components/cloudinary.jsx
+++ UriReduction.API/wwwroot/js/app/components/cloudinary.jsx
@@ -1,15 +1,2 @@
-const React = require('react');
-const {CloudinaryContext,Image,Video,Transformation} = require('cloudinary-react');
+const React = require ('react');
 
-class Cloudinary extends React.Component{
-    render(){
-        return (
-        <CloudinaryContext cloudName="dwvdvjlas">
-            <div>
-                <Image publicId="sample" width="300"/>
-            </div>
-        </CloudinaryContext>);
-    }
-}
-
-module.exports = Cloudinary;
