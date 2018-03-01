7b5e4942eff63c43965ed6d9ecd996d02b986fe5
g.shuksto <g.shuksto@itransition.com>
2018-03-01 16:15:09 +0300

    writed Cloudinary class

---
+++ UriReduction.API/wwwroot/js/app/components/cloudinary.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+
+class Cloudinary extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.inputStyle = {
+            display:'none'
+        }
+    }
+
+    render(){
+        return (
+        <div>
+            <input id='fileElem' type='file' style={this.inputStyle} onChange={this.props.onChange} />
+            <a href="#" onClick={this.props.onClick}>Select Image</a>
+        </div>);
+    }
+}
