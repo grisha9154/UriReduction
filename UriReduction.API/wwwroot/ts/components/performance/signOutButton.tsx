ee3d1d1dae7cb30a8a3cc9c64b42382d12bca815
g.shuksto <g.shuksto@itransition.com>
2018-03-22 18:43:35 +0300

    Updated signout button

--- UriReduction.API/wwwroot/ts/components/performance/signOutButton.tsx
+++ UriReduction.API/wwwroot/ts/components/performance/signOutButton.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import { FlatButton } from "material-ui";
+import * as $ from "jquery";
 
 class SignOutButton extends React.Component<any,any> {
     constructor(props:any) {
         super(props);
         this.onClick = this.onClick.bind(this);
     }
-    deleteCookie( name:string ):void {
-        document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-      }
     onClick():void {
-        this.deleteCookie(".AspNetCore.Identity.Application");
-        this.props.onClick();
+        $.ajax({
+            url:"/signout",
+            method:"GET",
+            contentType:"application/json",
+            success:(result:any)=> {
+                this.props.onClick();
+            }
+        });
     }
     render(): JSX.Element {
         return (<FlatButton label="Sign Out" onClick={this.onClick}/>);
